3d76428c5ad7f5ced7724e490e1c050e503f4c54
Edwin Nemeguen <95711147+PhoenixDwarf@users.noreply.github.com>
2025-06-04 21:18:47 -0500

    feat: restructure app layout, add routing for authentication and country pages, and enhance form components

--- src/app/shared/components/side-menu/side-menu.component.ts
+++ src/app/shared/components/side-menu/side-menu.component.ts
@@ -1,8 +1,38 @@
 import { Component } from '@angular/core';
+import { reactiveRoutes } from '../../../reactive/reactive.routes';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuItem {
+  title: string;
+  route: string;
+}
+
+const reactiveItems = reactiveRoutes[0].children ?? [];
 
 @Component({
   selector: 'app-side-menu',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu.component.html',
 })
-export class SideMenuComponent {}
+export class SideMenuComponent {
+  reactiveMenu: MenuItem[] = reactiveItems
+    .filter((route) => route.path !== '**')
+    .map((route) => ({
+      route: `reactive/${route.path}`,
+      title: `${route.title}`,
+    }));
+
+  authMenu: MenuItem[] = [
+    {
+      title: 'Registro',
+      route: './auth',
+    },
+  ];
+
+  countryMenu: MenuItem[] = [
+    {
+      title: 'Paises',
+      route: './country',
+    },
+  ];
+}
