42a460b5185a88904f3e1ab36c15aeef6bd8cdf9
Edwin Nemeguen <95711147+PhoenixDwarf@users.noreply.github.com>
2025-06-14 09:30:23 -0500

    feat: implement reactive form with validation for switches page and enhance form structure

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,9 +1,32 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-switches-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './switches-page.component.html',
 })
-export class SwitchesPageComponent {}
+export class SwitchesPageComponent {
+  private readonly fb = inject(FormBuilder);
+  formUtils = FormUtils;
+
+  myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    wantNotifications: [true, Validators.required],
+    termsAndConditions: [false, Validators.requiredTrue],
+  });
+
+  onSubmit() {
+    console.log(this.myForm.value);
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+    }
+  }
+}
